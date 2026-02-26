373a9e8a5be50ac79fc0c5ee99eb362c6e1b409d
Wairikibiku <denniswairikibiku@gmail.com>
2026-02-26 15:51:20 +0300

    deployment

--- tech-project/src/App.jsx
+++ tech-project/src/App.jsx
@@ -1,13 +1,11 @@
-
+ import React from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div style={{ textAlign: "center", padding: "50px" }}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,17 +14,20 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <h1>Welcome to My React + Vite App!</h1>
+      <p>
+        This is some sample content to show on your site. You can edit
+        <code>src/App.jsx</code> to add your own content.
       </p>
-    </>
-  )
+
+      <div style={{ marginTop: "30px", fontStyle: "italic" }}>
+        React + Vite deployed successfully on Vercel 🚀
+      </div>
+    </div>
+  );
 }
+
+
 
 export default App
